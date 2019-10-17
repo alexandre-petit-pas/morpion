9b65f01470cc175ee98ea5f0579c323efc9acb76
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-17 15:19:18 +0200

    Ajout app.rb

--- app.rb
+++ app.rb
@@ -1,3 +1,6 @@
-require_relative 'lib/game.rb' #On a besoin de Game.rb
-#On lance le jeux!!
+#On appelle notre fichier game.rb présent dans le dossier lib
+require_relative 'lib/game.rb'
+
+#On lance un nouveau jeu
 Game.new.go
+
