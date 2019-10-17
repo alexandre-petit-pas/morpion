0a89a0fd102658f3c22a1237eafc1021664f296b
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-17 15:23:33 +0200

    ajout player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -2,17 +2,17 @@ class Player
     attr_accessor :nom, :valeur
 
     def initialize(nom= nil, valeur= nil)
-      #Si on initialize sans un nom, on demande à l'utilisateur de taper son nom
+      #S'il n'y a pas de nom de base, on demande au joueur de rentrer un nom
       if nom == nil
         puts "Quel est votre nom de guerre ?"
         nom = gets.chomp
       end
       @nom = nom
-      #Si on initialize sans valeur, on demande à l'utilisateur de choisir un
+      #S'il n'y a pas de valeur de base, on demande au joueur de choisir entre la valeur X ou la valeur O
       if valeur == nil
         puts "Choisissez entre X ou O, avec quoi préfèrez-vous jouer ?"
         prise = gets.chomp
-        while prise != "X" && prise != "O" #Si l'utilisateur choisi un autre valeur que X et O
+        while prise != "X" && prise != "O" #Si l'utilisateur choisi une autre valeur que X et O, on lui retourne un message d'erreur
         puts "Non non non ! Tu dois choisir entre X ou O"
         prise = gets.chomp
         end
