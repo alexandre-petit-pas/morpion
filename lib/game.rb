161a43783c4166f5e3ad0f1eb88a5b36667e5f17
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-17 15:23:06 +0200

    ajout game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -11,7 +11,7 @@ class Game
      ██║██║   ██║██║   ██║█████╗  ██║   ██║██████╔╝    ╚██║
 ██   ██║██║   ██║██║   ██║██╔══╝  ██║   ██║██╔══██╗     ██║
 ╚█████╔╝╚██████╔╝╚██████╔╝███████╗╚██████╔╝██║  ██║     ██║
- ╚════╝  ╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝     ╚═╝".colorize(:pink)
+ ╚════╝  ╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝     ╚═╝".pink
 
       @joueur_1 = Player.new
 
@@ -21,64 +21,66 @@ class Game
      ██║██║   ██║██║   ██║█████╗  ██║   ██║██████╔╝     █████╔╝
 ██   ██║██║   ██║██║   ██║██╔══╝  ██║   ██║██╔══██╗    ██╔═══╝
 ╚█████╔╝╚██████╔╝╚██████╔╝███████╗╚██████╔╝██║  ██║    ███████╗
- ╚════╝  ╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝    ╚══════╝"
+ ╚════╝  ╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝    ╚══════╝".light_blue
 
-      if @joueur_1.valeur == "X"
+      if @joueur_1.valeur == "X" #si le joueur1 choisi X alors on assigne automatiquement "O" au joueur2
         @joueur_2 = Player.new(nil, "O")
       else
         @joueur_2 = Player.new(nil, "X")
       end
 
-      @board = Board.new
+      @board = Board.new #on affiche le plateau
 
     end
 
+    # On lance la partie
     def go
-      # On lance la partie,
-      puts "\n #{@joueur_1.nom} va jouer le premier"
-      sleep 2
-      turn #On fait appel à la méthode turn ci-dessous
+      puts "Notre soldat #{@joueur_1.nom} va jouer en premier" #on indique quel joueur joue en premier
+      sleep 1 #permet d'avoir un petit temps d'attente de 1sec
+      turn # On fait appel à la def turn ci-dessous
     end
 
+    #On affiche les cases du plateau numérotées
     def turn
-      #On affiche les cases avec ses numéros
       tab = [["1", "2", "3"],["4", "5", "6"],["7", "8", "9"] ]
-      puts "Les cases sont numérotés de manière suivant: "
+      puts "Les cases sont numérotées de la manière suivante : "
       tab.each do |x|
         puts "\t\t\t --- --- ---"
         puts "\t\t\t| "+x[0].to_s+" | "+x[1]+" | "+x[2]+" |"
         end
       puts "\t\t\t --- --- ---"
-      sleep 3
+      sleep 1
 
       boucle = true
       var_intermediaire = 0
       nbr_tour = 1
-      #On tourne le boucle
+
       while boucle
-        #Si le nombre de tour est paire, alors c'est le deuxième joueur qui joue
-        sleep 2 #Petit pause
-        if nbr_tour % 2 == 0
-          puts "\n#{@joueur_2.nom}, vous allez vous placez où? (case entre 1 à 9)\n"
+        sleep 1
+        if nbr_tour % 2 == 0 #Si le tour actuel est pair alors c'est le joueur 1 qui joue, sinon c'est le joueur 2
+          puts "Soldat #{@joueur_2.nom}, où souhaitez-vous vous placer ? (choisir une case entre 1 et 9)"
         else
-          puts "\n#{@joueur_1.nom}, vous allez vous placez où? (case entre 1 à 9)\n"
+          puts "Soldat #{@joueur_1.nom}, où souhaitez-vous vous placer ? (choisir une case entre 1 et 9)"
         end
-        #On place dans var_intermediaire le numero de case où le joueur veux placer
+
+        #On place dans var_intermediaire le numéro de la case dans laquelle le joueur veut placer sa valeur
         var_intermediaire = gets.chomp
         var_intermediaire = var_intermediaire.to_i
-        #Les numéros de case doivent être compris entre 1 à 9
+
+        # On indique que le numéro de la case choisie doit être compris entre 1 et 9, sinon on retourne un message d'erreur ainsi qu'un gets.chomp pour que le joueur retente sa chance
         while (var_intermediaire < 1) || (9 < var_intermediaire)
-          puts "Error, mauvaise endroit. Reessayez ! "
+          puts "Erreur ! Case invalide, réessayez."
           var_intermediaire = gets.chomp
           var_intermediaire = var_intermediaire.to_i
         end
-        #On place la valeur du joueur au numéros du case choisi
+
+        #On place la valeur du joueur dans la case choisie
         if nbr_tour % 2 == 0
           playing = @board.play(@joueur_2.valeur, var_intermediaire)
         else
           playing = @board.play(@joueur_1.valeur, var_intermediaire)
         end
-        #On check si un joueur a placé dans un placement non-vide
+        #On vérifie que la case choisie est bien vide, sinon ça retourne un message d'erreur présent dans le fichier board.rb
         while playing == false
           var_intermediaire = gets.chomp
           var_intermediaire = var_intermediaire.to_i
@@ -88,22 +90,23 @@ class Game
             playing = @board.play(@joueur_1.valeur, var_intermediaire)
           end
         end
-        #On affiche le board à chaque fois
+
         sleep 1
-        @board.to_s
-        #On check si quelqu'un a gagné
+        @board.to_s #On ré-affiche le plateau
+
+        # Ici, on vérifie si l'un des deux joueurs à gagné
         if @board.victory? == true
           if nbr_tour % 2 == 0
-            puts "#{@joueur_2.nom} a gagner!"
+            puts "#{@joueur_2.nom} sort de ce combat victorieux ! Bravo !"
           else
-            puts "#{@joueur_1.nom} a gagner!"
+            puts "#{@joueur_1.nom} sort de ce combat victorieux ! Bravo !"
           end
         end
-        #On affiche le vainqueur et arrête la boucle si on a une victoire
+        #On arrête la boucle en cas de victoire
         break if @board.victory? == true
-        #On incrémente le nombre de tour sinon
+        #Si ce n'est pas le cas, on incrémente le nombre de tour
         nbr_tour += 1
-        #On recommence le jeux si le nombre de tour dépasse 9 et si personne n'a gagné
+        #On recommence le jeu si le nombre de tour dépasse 9 et si aucun joueur n'a gagné
         if nbr_tour == 10
           puts "Match nul, on recommence!"
           nbr_tour = 1
@@ -116,3 +119,34 @@ class Game
     end
 
   end
+
+  class String #Classe permettant de changer la couleur de string
+  # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
+end
