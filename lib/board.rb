172941d3ebdf74583fe0447dfb5d2987d5082f53
Juliette Petit-Pas <petitpasjuliette@gmail.com>
2019-10-17 15:21:31 +0200

    ajout board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -16,7 +16,8 @@ class Board
         @case8 = BoardCase.new("8", 8)
         @case9 = BoardCase.new("9", 9)
 
-        @array_case = [[@case1, @case2, @case3],[@case4, @case5, @case6],[@case7, @case8, @case9]] #Puis on range ces 9 cases dans un array dédié, qu'on a aussi défini en attr_accessor
+        #Puis on range ces 9 cases dans un array dédié, qu'on a aussi défini en attr_accessor
+        @array_case = [[@case1, @case2, @case3],[@case4, @case5, @case6],[@case7, @case8, @case9]]
     end
 
     #Méthode qui affiche notre plateau
@@ -28,81 +29,82 @@ class Board
             puts "\t\t\t --- --- ---"
     end
 
-    #On définie une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X, ou O)
+    #On définie une méthode qui change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
     def play(valeur, case_num)
-      case case_num #On switch le case_num qui est le numero de la case
-      #On vérifi d'abord si les emplacements ne sont pas vides
+
+      #On vérifie que la case séléctionnée n'est pas déjà prise, si c'est le cas, on retourne un message d'erreur au joueur
+      case case_num
       when 1
         if @array_case[0][0].to_s == "1".to_s
           @array_case[0][0] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide! Choisissez un autre: "
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 2
         if @array_case[0][1].to_s == "2".to_s
           @array_case[0][1] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide! Choisissez un autre: "
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 3
         if @array_case[0][2].to_s == "3".to_s
           @array_case[0][2] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide! Choisissez un autre: "
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 4
         if @array_case[1][0].to_s == "4".to_s
           @array_case[1][0] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide! Choisissez un autre: "
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 5
         if @array_case[1][1].to_s == "5".to_s
           @array_case[1][1] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide! Choisissez un autre: "
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 6
         if @array_case[1][2].to_s == "6".to_s
           @array_case[1][2] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide! Choisissez un autre: "
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 7
         if @array_case[2][0].to_s == "7".to_s
           @array_case[2][0] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide!"
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 8
         if @array_case[2][1].to_s == "8".to_s
           @array_case[2][1] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide!"
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       when 9
         if @array_case[2][2].to_s == "9".to_s
           @array_case[2][2] = valeur
         else
-          puts "Erreur, l'emplacement n'est pas vide!"
+          puts "Cet emplacement est déjà pris, choisissez-en un autre :"
           return false
         end
       end
 
     end
 
-    #Méthode qui vérifie si un joueur a gagné
+    #Méthode qui vérifie si le joueur a gagné
     def victory?
       if
-      #Les cas possibles de victoire
+      # Ici, on affiche tous les cas possibles de victoire
         (@array_case[0][0] == @array_case[0][1]) && (@array_case[0][1] == @array_case[0][2]) ||
         (@array_case[1][0] == @array_case[1][1]) && (@array_case[1][1] == @array_case[1][2]) ||
         (@array_case[2][0] == @array_case[2][1]) && (@array_case[2][1] == @array_case[2][2]) ||
@@ -111,7 +113,7 @@ class Board
         (@array_case[0][2] == @array_case[1][2]) && (@array_case[1][2] == @array_case[2][2]) ||
         (@array_case[0][0] == @array_case[1][1]) && (@array_case[1][1] == @array_case[2][2]) ||
         (@array_case[0][2] == @array_case[1][1]) && (@array_case[1][1] == @array_case[2][0])
-        true #Si il y a une victoire, on retourne true
+        true #S'il y a victoire, on retourne true
       else
         false #Si ce n'est pas le cas, on retourne false
       end
